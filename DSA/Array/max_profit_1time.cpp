b8a4e2941a80c8b0d75e35b338f48ca6a8d4ad25
Ahir <nahir@csus.edu>
2026-01-18 22:50:00 -0800

    updating max_profit_1time.cpp

--- DSA/Array/max_profit_1time.cpp
+++ DSA/Array/max_profit_1time.cpp
@@ -5,30 +5,18 @@
 int maximumProfit(std::vector<int> &prices) {
     int n = prices.size();
     int min_price = prices[0];
-    int max_price = prices[0];
     int max_profit = 0;
-    std::vector<int> MAX_PROFITE = {};
         
-    for (int i = 0; i<n-1; i++){
-        if (prices[i+1]>prices[i]){
-            max_price = std::max(max_price, prices[i+1]);
-                           
-        }else(prices[i+1]<=prices[i]);{
-            max_profit = max_price - min_price;
-            MAX_PROFITE.push_back(max_profit);
-            min_price = prices[i+1];
-            max_price = prices[i+1];
-            continue;
-        }
+    for (int i = 1; i < n; i++){
+        min_price = std::min(min_price, prices[i]);
+        max_profit = std::max(max_profit, prices[i] - min_price);
     }
-    int ans = *std::max_element(MAX_PROFITE.begin(), MAX_PROFITE.end());
-    return ans;
+    
+    return max_profit;
 }
 
 int main(){
-
-    std::vector<int> prices = {7, 10, 1, 3, 6, 9, 2};
+    std::vector<int> prices = {7, 1, 5, 3, 6, 4};
     std::cout<<maximumProfit(prices);
-
     return 0;
 }
