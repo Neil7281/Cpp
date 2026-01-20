332e67e4503c8d80110cea90ab6c0cb93531f3a8
Neel_Ahir <nsahir04@gmail.com>
2026-01-20 00:35:29 -0800

    updating get_min_difference

--- DSA/Array/get_min_difference.cpp
+++ DSA/Array/get_min_difference.cpp
@@ -5,12 +5,11 @@
 int getMinDiff(std::vector<int> &arr, int k) {
     int n = arr.size();
     std::sort(arr.begin(), arr.end());
-    
     int result = arr[n-1] - arr[0];  // Initial difference
     
     // Try all split points
     for (int i = 0; i < n - 1; i++) {
-        int minVal = std::min(arr[0] + k, arr[i+1] - k);
+        int minVal = std::min(arr[0] + k, arr[i+1] - k); //get the min value among arr after operation
         int maxVal = std::max(arr[n-1] - k, arr[i] + k);
         
         if (minVal >= 0) {  // Ensure no negative heights
