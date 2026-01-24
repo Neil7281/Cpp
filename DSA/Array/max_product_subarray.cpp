29a872d06dd6a5c82f2cba34e1d6072462520f27
Neel_Ahir <nsahir04@gmail.com>
2026-01-23 23:33:34 -0800

    updating max_product_subarray.cpp

--- DSA/Array/max_product_subarray.cpp
+++ DSA/Array/max_product_subarray.cpp
@@ -2,24 +2,20 @@
 #include<vector>
 
 int maxSubarraySum(std::vector<int> &arr) {
-        int n = arr.size();
-        int res = arr[0];   //max subarray sum 
+        int n = prices.size();
+    int min_price = prices[0];
+    int max_profit = 0;
         
-        int maxEnding = arr[0];
-        
-        for(int i = 1; i<n; i++){
-            
-            maxEnding = std::max(arr[i], maxEnding + arr[i]);
-            
-            res = std::max(res, maxEnding);
-        }
-        
-        return res;
+    for (int i = 1; i < n; i++){
+        min_price = std::min(min_price, prices[i]);
+        max_profit = std::max(max_profit, prices[i] - min_price);
     }
+    
+    return max_profit;
+}
 
-    int main(){
-
-        std::vector<int> vec = {2, 3, -8, 7, -1, 2, 3};
-        std::cout<<"maximum subarray sum = " << maxSubarraySum(vec);
-
-    }
+int main(){
+    std::vector<int> prices = {7, 1, 5, 3, 6, 4};
+    std::cout<<maximumProfit(prices);
+    return 0;
+}
